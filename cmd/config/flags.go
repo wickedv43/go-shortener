5f6f31b8136c55cd2a7822c8b42ab85768abbab5
vetersuka <wickedv43@gmail.com>
2024-09-02 21:30:29 +0300

    env variables

--- cmd/config/flags.go
+++ cmd/config/flags.go
@@ -2,18 +2,30 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
-// переменная FlagRunAddr содержит адрес и порт для запуска сервера
+// FlagRunAddr server ip:port
+// FlagSuffixAddr address before shortered
 var (
 	FlagRunAddr    string
 	FlagSuffixAddr string
 )
 
-// ParseFlags обрабатывает аргументы командной строки
-// и сохраняет их значения в соответствующих переменных
+// Parse arguments from env or flags
 func ParseFlags() {
 	flag.StringVar(&FlagRunAddr, "a", ":8080", "address and port to run server")
 	flag.StringVar(&FlagSuffixAddr, "b", "http://localhost:8080", "address before short url")
+
+	ServerAddr := os.Getenv("SERVER_ADDRESS")
+	if ServerAddr != "" {
+		FlagRunAddr = ServerAddr
+	}
+
+	BaseURL := os.Getenv("BASE_URL")
+	if BaseURL != "" {
+		FlagSuffixAddr = BaseURL
+	}
+
 	flag.Parse()
 }
