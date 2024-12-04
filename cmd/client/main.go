14fe1882d72e6332266fde60e87f7130b1ce3cfe
vetersuka <wickedv43@gmail.com>
2024-12-04 18:12:37 +0300

    changes

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bytes"
 	"compress/gzip"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -27,16 +25,16 @@ func main() {
 
 	var r Request
 
-	bidy := `[ 
-{
-        "correlation_id": "<11>",
-        "original_url": "fff"
-    },
-{
-        "correlation_id": "<12>",
-        "original_url": "ff3"
-    }
-]`
+	//	bidy := `[
+	//{
+	//        "correlation_id": "<11>",
+	//        "original_url": "fff"
+	//    },
+	//{
+	//        "correlation_id": "<12>",
+	//        "original_url": "ff3"
+	//    }
+	//]`
 
 	_, err := json.Marshal(r)
 	if err != nil {
@@ -44,13 +42,15 @@ func main() {
 	}
 	fmt.Println(r)
 
-	req, err := http.NewRequest("POST", "http://localhost:8080/api/shorten/batch", bytes.NewReader([]byte(bidy)))
-	if err != nil {
-		err = errors.New("client post")
-		fmt.Println(err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Add("Accept-Encoding", "gzip")
+	//req, err := http.NewRequest("POST", "http://localhost:8080/api/shorten/batch", bytes.NewReader([]byte(bidy)))
+	//if err != nil {
+	//	err = errors.New("client post")
+	//	fmt.Println(err)
+	//}
+	//req.Header.Set("Content-Type", "application/json")
+
+	req, err := http.NewRequest("GET", "http://localhost:8080/sGlwJpHN", nil)
+	//req.Header.Add("Accept-Encoding", "gzip")
 
 	res, err := client.Do(req)
 	if err != nil {
