08d4fab50f9f586f131b75e3999ca0dc941a2bb1
vetersuka <wickedv43@gmail.com>
2024-12-04 23:25:43 +0300

    test

--- internal/server/server.go
+++ internal/server/server.go
@@ -29,7 +29,7 @@ func NewServer(i do.Injector) (*Server, error) {
 	}
 
 	e := gin.New()
-	e.Use(gin.Recovery(), server.logHandler())
+	e.Use(gin.Recovery(), server.logHandler(), server.gzipMiddleware(), server.CORSMiddleware())
 
 	server.engine = e
 	server.cfg = cfg
