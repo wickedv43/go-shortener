bb1fc86dee65ad9e5e8f36b25ec977b4fd4569e5
vetersuka <wickedv43@gmail.com>
2025-02-06 00:02:37 +0300

    check

--- internal/config/config.go
+++ internal/config/config.go
@@ -33,7 +33,7 @@ type Logger struct {
 
 func NewConfig(_ do.Injector) (*Config, error) {
 	var cfg Config
-	flag.StringVar(&cfg.Server.FlagRunAddr, "a", "localhost:8080", "address and port to run server")
+	flag.StringVar(&cfg.Server.FlagRunAddr, "a", ":8080", "address and port to run server")
 	flag.StringVar(&cfg.Server.FlagSuffixAddr, "b", "http://localhost:8080", "address before short url")
 	flag.StringVar(&cfg.Server.FlagStoragePath, "f", "./db/storage.json", "path to database file")
 	flag.StringVar(&cfg.Server.FlagDatabaseDSN, "d", "", "database connection string")
