6af52ec99acdc698eeb5e2cd998e97e61f3bea02
vetersuka <wickedv43@gmail.com>
2025-02-16 18:01:47 +0300

    test

--- internal/storage/postgres.go
+++ internal/storage/postgres.go
@@ -37,7 +37,7 @@ func NewPostgresStorage(i do.Injector) (*PostgresStorage, error) {
 
 	query := `
     CREATE TABLE IF NOT EXISTS urls (
-        uuid SERIAL PRIMARY KEY,
+        uuid SERIAL NOT NULL,
         short_url TEXT NOT NULL,
         original_url TEXT NOT NULL
     );`
